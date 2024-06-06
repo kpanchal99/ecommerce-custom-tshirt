6b05cfd65e5432bcfdffee759ea9d9ae9956d11e
karanp29 <karanpanchal.git@gmail.com>
2024-06-06 15:33:30 +0530

    added CartCheckout

---
+++ ecommerce_website/CartCheckout.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ecommerce_website
+{
+    public partial class CartCheckout : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // get data from session 
+            // cart count
+            // call each proper 
+        }
+    }
+}
